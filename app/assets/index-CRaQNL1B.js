e382622bf1f91cb3613843540426139f0219e208
GNUTUX <salehoukiki@gmail.com>
2026-05-23 21:28:00 +0100

    Add files via upload

---
+++ app/assets/index-CRaQNL1B.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-DaNIGYXk.js","./index-B_vY8pw-.js","./index-nolZCsy5.css"])))=>i.map(i=>d[i]);
+import{r,_ as p}from"./index-B_vY8pw-.js";const o=r("App",{web:()=>p(()=>import("./web-DaNIGYXk.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{o as App};
