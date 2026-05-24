4552eeee324f6635013d34b332ac550b715bfb69
GNUTUX <salehoukiki@gmail.com>
2026-05-24 19:06:02 +0100

    Add files via upload

---
+++ apps/web/android/app/src/main/assets/public/assets/index-DI0atKPt.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-nZo4XONM.js","./index-C7szKXdK.js","./index-x--2hhc0.css"])))=>i.map(i=>d[i]);
+import{r,_ as p}from"./index-C7szKXdK.js";const o=r("App",{web:()=>p(()=>import("./web-nZo4XONM.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{o as App};
